9d9acbe8551821839dd8a61e031b9b09ae71db52
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-03-09 20:13:03 -0300

    Changes: * Create cron job fuction done * Check root: echo translated to portuguese * Generate log file: Fixed print of the tenth element * Generate html file: Strong tags added

--- functions.sh
+++ functions.sh
@@ -4,17 +4,28 @@ source info_list.sh
 
 check_root () {
     if [[ $EUID -ne 0 ]]; then
-        echo "This script must be run as root!!"
+        echo "Esse script deve ser executado como root, saindo..."
         exit 1
     fi
+}
+
+create_cron_job () {
+    CURRENT_PATH=$(pwd)
+    echo "Creating Cron job..."
+    crontab -l > tempfile
+    echo "*/2 * * * * /bin/bash ${CURRENT_PATH}/init.sh > ${CURRENT_PATH}/cron.out 2>&1" >> tempfile
+    crontab tempfile
+    rm tempfile
+    echo "Cron job created!!"
 }
 
 generate_log_file () {
     if [ ! -f 'log.txt' ]; then
         $(touch log.txt)
     fi
-
-    echo -e "$1\n$2\n$3\n$4\n$5\n$6\n$7\nEOF" >> log.txt
+    echo "Creating log file..."
+    echo -e "${1}\n${2}\n${3}\n${4}\n${5}\n${6}\n${7}\n${8}\n${9}\n${10}\nEOF" >> log.txt
+    echo "Log file created!!"
 }
 
 generate_html_file () {
@@ -22,6 +33,8 @@ generate_html_file () {
     if [ ! -f '${HTML_PATH}/index.html' ]; then
         $(touch ${HTML_PATH}/index.html)
     fi
+
+    echo "Creating HTML file..."
 
     cat > ${HTML_PATH}/index.html << EOF
 <!DOCTYPE html>
@@ -46,7 +59,7 @@ generate_html_file () {
   <section class="section">
     <div class="content">
 
-      <article class="message is-dark">
+      <article class="message is-info">
         <div class="message-header">
           <p>Data e horário da coleta: ${CURRENT_TIME}</p>
         </div>
@@ -61,10 +74,10 @@ generate_html_file () {
 
             <p><strong>Processo que mais utiliza RAM:</strong></p>
               <ul>
-                <li>PID: ${PID_PROCESS_USES_MORE_RAM}</li>
-                <li>Nome: ${NAME_PROCESS_USES_MORE_RAM}</li>
-                <li>Percentual de RAM utilizada: ${PERCENT_PROCESS_USES_MORE_RAM}%</li>
-                <li>RAM utilizada em Kilobytes: ${RSS_PROCESS_USES_MORE_RAM}KB</li>
+                <li><strong>PID:</strong> ${PID_PROCESS_USES_MORE_RAM}</li>
+                <li><strong>Nome:</strong> ${NAME_PROCESS_USES_MORE_RAM}</li>
+                <li><strong>Percentual de RAM utilizada:</strong> ${PERCENT_PROCESS_USES_MORE_RAM}%</li>
+                <li><strong>RAM utilizada em Kilobytes:</strong> ${RSS_PROCESS_USES_MORE_RAM}KB</li>
               </ul>
 
             <p><strong>Partições:</strong></p>
@@ -85,4 +98,6 @@ generate_html_file () {
   </body>
 </html>
 EOF
+
+echo "HTML file created!!"
 }
--- init.sh
+++ init.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-source info_list.sh
-source functions.sh
+cd "$(dirname "$0")" # cd para pasta do script, para não dar problema com o cron
+
+CURRENT_PATH=$(pwd)
+
+source ${CURRENT_PATH}/info_list.sh
+source ${CURRENT_PATH}/functions.sh
 echo "Initializing..."
 
 check_root
@@ -9,10 +13,8 @@ check_root
 echo "Collecting data..."
 echo "Data collected!"
 
-echo "Creating log file..."
 generate_log_file "$CURRENT_TIME" "$HOST_NAME" "$CURRENT_USED_RAM" "$CURRENT_FREE_RAM" "$PID_PROCESS_USES_MORE_RAM" "$NAME_PROCESS_USES_MORE_RAM" "$PERCENT_PROCESS_USES_MORE_RAM" "$RSS_PROCESS_USES_MORE_RAM" "$PARTITIONS" "$INTERFACES"
-echo "Log file created!!"
 
-echo "Creating HTML file..."
 generate_html_file
-echo "HTML file created!!"
+
+create_cron_job
